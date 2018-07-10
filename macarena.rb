a5306b9e466b949fb3d355d8b4c3626bc9e25f4f
sophiahusak <72futbolplayer@gmail.com>
2018-07-10 17:34:52 +0000

    automatically backed up by learn

--- macarena.rb
+++ macarena.rb
@@ -12,19 +12,19 @@ puts "Step 1: Right arm out in front of you, palm facing down."
 puts "Step 2: Left arm out in front of you, palm facing down."Turn your right palm up."
 
 # Step 3: FIXME
-puts "Step 3: Turn your right palm up."Place your right hand on your left shoulder."
+puts "Step 3: Turn your right palm up."
 
 # Step 4: FIXME
 puts "Step 4: Turn your left palm up."
 
 # Step 5: FIXME
-puts "Step 5: Turn your right palm up."Bring your right hand up and around to the back of your head."
+puts "Step 5: Place your right hand on your left shoulder."
 
 # Step 6: FIXME
 puts "Step 6: Left arm out in front of you, palm facing down."
 
 # Step 7: FIXME
-puts "Step 7: Place your left hand on your left shoulder."
+puts "Step 7: Bring your right hand up and around to the back of your head."Place your left hand on your left shoulder."
 
 # Step 8: FIXME
 puts "Step 8: The Last Step"
